d4874e47dcbcbb90a7a84f183ed2796fb432950b
Andres Herrera <herrera3231@yahoo.com>
2019-10-28 19:12:14 -0700

    add routing

--- weekday/src/app/app-routing.module.ts
+++ weekday/src/app/app-routing.module.ts
@@ -1,11 +1,36 @@
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
+import { signupRouterConfig } from './routing/signup-router-config';
+
+const indexRoute: Route = {
+    path: '',
+    component: LoginComponent
+};
+
+const fallbackRoute: Route = {
+    path: '**',
+    component: LoginComponent
+};
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    ...signupRouterConfig,
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    indexRoute,
+    fallbackRoute
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- weekday/src/app/app.module.ts
+++ weekday/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { InviteTeamMembersComponent } from './pages/invite-team-members/invite-team-members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SignupComponent,
+    InviteTeamMembersComponent
   ],
   imports: [
     BrowserModule,
